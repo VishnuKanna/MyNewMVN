86d11d36ea00f55a64e9e6dd51daac423b48e80e
VishnuKanna <vishnumas111@gmail.com>
2022-07-05 22:37:06 +0530

    Added

---
+++ src/main/java/com/vishnukanna/App.java
@@ -0,0 +1,11 @@
+package com.vishnukanna;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello There !!!!, You are Inside My Application!!!!" );
+        
+    }
+}
